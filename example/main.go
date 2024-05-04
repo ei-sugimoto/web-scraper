6f60034c2b27337947c2b6435abc01d7a1ab09bc
ei-sugimoto <eikun1014@gmail.com>
2024-05-04 23:16:19 +0900

    module名変更

--- example/main.go
+++ example/main.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"fmt"
+    "github.com/ei-sugimoto/webscraper"
 )
 
 func main() {
-	var result []string = Scrape("https://zenn.dev/books/explore")
-	fmt.Println(result)
+    var result []string = .Scrape("https://zenn.dev/books/explore")
+    fmt.Println(result)
 }
